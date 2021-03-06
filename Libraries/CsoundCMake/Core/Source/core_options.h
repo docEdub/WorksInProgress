aeadcc6c4e881cfbaa54d3305ab126b3752934a8
Dr. Ewww <drew@docEdub.com>
2021-03-06 14:33:55 -0500

    Move .git-lfs submodule path to LargeFiles

--- Libraries/CsoundCMake/Core/Source/core_options.h
+++ Libraries/CsoundCMake/Core/Source/core_options.h
@@ -1,6 +1,6 @@
 
 --env:INCDIR=${CSOUND_CMAKE_PREPROCESSED_FILES_DIR}
---env:SSDIR=${ROOT_DIR}/.git-lfs
+--env:SSDIR=${ROOT_DIR}/LargeFiles
 --messagelevel=${CSOUND_MESSAGE_LEVEL}
 --midi-device=0
 --nodisplays
