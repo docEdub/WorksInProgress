390c352311a1340721fda968982bfa7ad856e230
Dr. Ewww <drew@docEdub.com>
2021-03-06 01:00:57 -0500

    Move Projects.cmake code to project's Csound CMakeLists.txt

--- Projects/Projects.cmake
+++
@@ -1,8 +0,0 @@
-
-get_filename_component(PROJECT_DIR "${CMAKE_SOURCE_DIR}/.." ABSOLUTE)
-get_filename_component(PROJECT_NAME "${PROJECT_DIR}" NAME)
-
-get_filename_component(ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/.." ABSOLUTE)
-
-include("${ROOT_DIR}/Libraries/CsoundCMake/CsoundCMake.cmake")
-find_package(CsoundCMake.Cabbage REQUIRED)
