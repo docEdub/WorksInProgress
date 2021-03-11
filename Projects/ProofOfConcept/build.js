9befe329dba784e8ff9b17879f429cd64a10b4f6
Dr. Ewww <drew@docEdub.com>
2021-03-11 05:42:33 -0500

    Use absolute paths in build.js

--- Projects/ProofOfConcept/build.js
+++ Projects/ProofOfConcept/build.js
@@ -1,34 +1,32 @@
 var spawn = require('child_process').spawn
 var os = require('os')
 var fs = require('fs')
+var path = require('path')
 
 if (os.type() === 'Darwin') {
-    const csoundDir = 'Csound'
-    const shell_buildDir = '_.build'
-    const shell_optionsFile = 'CMakeOptions.cmake'
-    const shell_cacheFile = shell_buildDir + '/CMakeCache.txt'
-    const js_optionsFile = csoundDir + '/' + shell_optionsFile
-    const js_cacheFile = csoundDir + '/' + shell_cacheFile
-    const js_optionsDefaultFile = '../../Libraries/CsoundCMake/CMakeOptions.default.cmake'
+    const csoundDir = path.resolve('Csound')
+    const buildDir = csoundDir + '/_.build'
+    const optionsFile = csoundDir + '/CMakeOptions.cmake'
+    const cacheFile = buildDir + '/CMakeCache.txt'
+    const optionsDefaultFile = path.resolve('../../Libraries/CsoundCMake/CMakeOptions.default.cmake')
 
-    if (!fs.existsSync(js_optionsFile)) {
-        fs.copyFileSync(js_optionsDefaultFile, js_optionsFile)
+    if (!fs.existsSync(optionsFile)) {
+        fs.copyFileSync(optionsDefaultFile, optionsFile)
     }
 
-    let shellCommand = 'cd ' + csoundDir
+    let command = 'cd ' + csoundDir
 
-    // If options file was modified after cache file was configured, delete cache file to make CMake remake it.
-    if (fs.existsSync(js_cacheFile)) {
-        if (fs.statSync(js_cacheFile).mtime < fs.statSync(js_optionsFile).mtime) {
-            shellCommand += ' && rm ' + shell_cacheFile
+    // If options file was modified after cache file was configured, delete cache file so CMakes remake it.
+    if (fs.existsSync(cacheFile)) {
+        if (fs.statSync(cacheFile).mtime < fs.statSync(optionsFile).mtime) {
+            command += ' && rm ' + cacheFile
         }
     }
 
-    shellCommand += ' && cmake -B ' + shell_buildDir + ' -C ' + shell_optionsFile + ' -D BUILD_PLAYBACK_CSD='
-    shellCommand += process.argv.includes('BUILD_PLAYBACK_CSD') ? 'ON' : 'OFF'
-    shellCommand += ' && cmake --build ' + shell_buildDir
+    command += ' && cmake -B ' + buildDir + ' -C ' + optionsFile + ' -D BUILD_PLAYBACK_CSD=' +
+        (process.argv.includes('BUILD_PLAYBACK_CSD') ? 'ON' : 'OFF') + ' && cmake --build ' + buildDir
 
-    spawn('bash', [ '-c', shellCommand ], { stdio: 'inherit' })
+    spawn('bash', [ '-c', command ], { stdio: 'inherit' })
 }
 else {
     throw new Error('Unsupported OS: ' + os.type())
