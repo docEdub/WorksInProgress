7face1b26c064cd9322f3e26436aa3909eb03c8d
Dr. Ewww <drew@docEdub.com>
2021-03-18 18:53:22 -0400

    Add CMake target for .csd files that aren't plugins

--- Libraries/CsoundCMake/Cabbage/CsoundCMake.CabbageConfig.cmake
+++ Libraries/CsoundCMake/Cabbage/CsoundCMake.CabbageConfig.cmake
@@ -38,10 +38,13 @@ set(Cabbage_UiOutlineGroups OFF CACHE BOOL)
 
 function(add_csd)
     add_csd_implementation(${ARGN} DEPENDS CsoundCMake.Cabbage)
+    set(csd "${ARGV0}")
+    get_filename_component(csd_file_name "${csd}" NAME)
+    add_custom_target("${csd_file_name}" ALL DEPENDS "${CSOUND_CMAKE_PLUGIN_OUTPUT_DIR}/${csd_file_name}")
 endfunction()
 
 function(export_csd_plugin)
-    add_csd("${ARGV0}")
+    add_csd_implementation(${ARGV0} DEPENDS CsoundCMake.Cabbage)
 
     set(in_file "${ARGV0}")
 
