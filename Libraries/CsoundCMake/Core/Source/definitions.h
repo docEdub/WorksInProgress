a8ef48d9bd6c45a0bfcb218a0bcda6c7a2e26a1e
Dr. Ewww <drew@docEdub.com>
2021-04-03 14:02:32 -0400

    Move EVENT definitions up so they're before LOG definitions

--- Libraries/CsoundCMake/Core/Source/definitions.h
+++ Libraries/CsoundCMake/Core/Source/definitions.h
@@ -66,6 +66,12 @@
 #define VOLUME_TRACKING_SIGNAL 0.000001
 #define VOLUME_TRACKING_SIGNAL_TO_VOLUME(signal) (signal * VOLUME_TRACKING_DIVISOR)
 
+#define EVENT_ALWAYS_ON 0
+#define EVENT_EFFECT_ON 1
+#define EVENT_NOTE_ON   1
+#define EVENT_NOTE_OFF  2
+#define EVENT_CC        3
+
 #define LOG_TRACE _(${CsoundLog_Level0Trace})
 #define LOG_DEBUG _(${CsoundLog_Level1Debug})
 #define LOG_INFO _(${CsoundLog_Level2Info})
@@ -77,12 +83,6 @@
 #define LOG_FILENAMES _(${CsoundLog_FileNames})
 #define LOG_TO_CSOUND_OUTPUT _(${CsoundLog_ToCsoundOutput})
 #define LOG_TO_DAW_SERVICE _(${CsoundLog_ToDawService})
-
-#define EVENT_ALWAYS_ON 0
-#define EVENT_EFFECT_ON 1
-#define EVENT_NOTE_ON   1
-#define EVENT_NOTE_OFF  2
-#define EVENT_CC        3
 
 #ifdef _MSC_VER
     #define ARGUMENT_COUNT(...)  INTERNAL_EXPAND_ARGS_PRIVATE(INTERNAL_ARGS_AUGMENTER(__VA_ARGS__))
