dd1e965761e49564ca8bdebe6222d9c850baa5d5
Dr. Ewww <drew@docEdub.com>
2021-05-26 19:21:49 -0400

    Add project.ts for easier copy/paste to BabylonJS playground

--- Projects/ProofOfConcept/BabylonJs/index.ts
+++ Projects/ProofOfConcept/BabylonJs/index.ts
@@ -1,7 +1,9 @@
 
 import * as BABYLON from "babylonjs";
+import { Project } from "./project";
 
 const createScene = async () => {
+
     // This creates and styles a canvas element on the html page
     const canvas = <HTMLCanvasElement>document.createElement('CANVAS');
     canvas.id = 'BabylonCanvas';
@@ -19,33 +21,10 @@ const createScene = async () => {
         engine.resize();
     });
     
-    // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new BABYLON.Scene(engine);
+    // This creates the scene
+    var scene = Project.CreateScene(engine, canvas);
 
-    // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-
-    // This targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
-
-    // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
-
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-
-    // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
-
-    // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
-
-    // Move the sphere upward 1/2 its height
-    sphere.position.y = 1;
-
-    // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-    BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
-
+    // This renders each frame of the scene
     engine.runRenderLoop(() => {
         scene.render();
     })
