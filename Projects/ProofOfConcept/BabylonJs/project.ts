a66b8be3d2f72562a4b7e7fea1c3ba3db8d93712
Dr. Ewww <drew@docEdub.com>
2021-05-26 20:21:35 -0400

    Get WebXR playground example working with Mozilla WebXR emulator

--- Projects/ProofOfConcept/BabylonJs/project.ts
+++ Projects/ProofOfConcept/BabylonJs/project.ts
@@ -2,39 +2,40 @@ import * as BABYLON from "babylonjs";
 
 
 
-class Playground {
-    public static CreateScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement): BABYLON.Scene {
+class Playground { public static CreateScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement): BABYLON.Scene {
 
-        // This creates a basic Babylon Scene object (non-mesh)
-        var scene = new BABYLON.Scene(engine);
+    // This creates a basic Babylon Scene object (non-mesh)
+    var scene = new BABYLON.Scene(engine);
 
-        // This creates and positions a free camera (non-mesh)
-        var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+    // This creates and positions a free camera (non-mesh)
+    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
 
-        // This targets the camera to scene origin
-        camera.setTarget(BABYLON.Vector3.Zero());
+    // This targets the camera to scene origin
+    camera.setTarget(BABYLON.Vector3.Zero());
 
-        // This attaches the camera to the canvas
-        camera.attachControl(canvas, true);
+    // This attaches the camera to the canvas
+    camera.attachControl(canvas, true);
 
-        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-        var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
 
-        // Default intensity is 1. Let's dim the light a small amount
-        light.intensity = 0.7;
+    // Default intensity is 1. Let's dim the light a small amount
+    light.intensity = 0.7;
 
-        // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-        var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+    // Our built-in 'sphere' shape.
+    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
 
-        // Move the sphere upward 1/2 its height
-        sphere.position.y = 1;
+    // Move the sphere upward 1/2 its height
+    sphere.position.y = 1;
 
-        // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-        BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+    scene.createDefaultEnvironment();
 
-        return scene;
-    }
-}
+    // XR
+    const xrHelper = scene.createDefaultXRExperienceAsync({});
+
+    return scene;
+
+}}
 
 
 
