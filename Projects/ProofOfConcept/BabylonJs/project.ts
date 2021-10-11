21a10f52704ad109852ef3a7a916036873c28df4
Dr. Ewww <drew@docEdub.com>
2021-10-10 22:17:42 -0400

    Set `animateCamera` flag to true

--- Projects/ProofOfConcept/BabylonJs/project.ts
+++ Projects/ProofOfConcept/BabylonJs/project.ts
@@ -26,7 +26,7 @@ class Playground { public static CreateScene(engine: BABYLON.Engine, canvas: HTM
     const logDebugMessages = true;
     const showGroundGrid = true;
 
-    let animateCamera = false;
+    let animateCamera = true;
     const csoundCameraUpdatesPerSecond = 10;
     const csoundIoBufferSize = 128;
     const groundSize = 1000;
