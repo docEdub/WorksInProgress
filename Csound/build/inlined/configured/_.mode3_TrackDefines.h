48246bac3090b92466cfbb36eec8f9939c55a841
Dr. Ewww <drew@docEdub.com>
2021-10-30 15:53:57 -0400

    Add files generated by DawService.csd

---
+++ Csound/build/inlined/configured/_.mode3_TrackDefines.h
@@ -0,0 +1,8 @@
+#define DistanceDelaySynth_0 4
+#define PointSynth_1 5
+#define GroundBubbleSynth_2 6
+#define AuxMixInstrument 7
+#define AuxInstrument 8
+#define Reverb_3_0 9
+#define MasterInstrument 10
+#define FinalMixInstrument 11
---
+++ Csound/build/inlined/configured/_.mode3_TrackSet.sco
@@ -0,0 +1,2 @@
+i Init_instrnum 0 -1 InstrumentCount(3) InstrumentIndexOffset(0) AuxCount(1) AuxIndexOffset(3)
+i CONCAT(Reverb_3_0, .1) 0 -1 EVENT_EFFECT_ON 0 0 Unused(0)
