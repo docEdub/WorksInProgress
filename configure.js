ad5002d4fae73a5bafc6684becd7c4d4b14107c0
Dr. Ewww <andy@docEdub.com>
2022-06-04 05:00:17 -0400

    Refactor configure.js

--- configure.js
+++ configure.js
@@ -2,14 +2,13 @@ var os = require('os');
 var path = require('path');
 var spawn = require('child_process').spawn;
 
+const csoundDir = path.resolve('Csound');
+const buildDir = path.join(csoundDir, 'build');
+
 if (os.type() === 'Darwin') {
-    const csoundDir = path.resolve('Csound');
-    const buildDir = csoundDir + '/build';
     spawn('bash', [ '-c', 'cmake -B ' + buildDir + ' -S ' + csoundDir ], { stdio: 'inherit' });
 }
 else if (os.type() === 'Windows_NT') {
-    const csoundDir = path.resolve('Csound');
-    const buildDir = path.join(csoundDir, 'build');
     spawn('cmd', [ '/k', 'cmake -B ' + buildDir + ' -S ' + csoundDir ], { stdio: 'inherit' });
 }
 else {
