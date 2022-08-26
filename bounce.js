8fa551a1c01d5ab0c6d0123e72969fb22f0bdf9b
Dr. Ewww <andy@docEdub.com>
2022-08-26 07:09:26 -0400

    Cleanup

--- bounce.js
+++ bounce.js
@@ -94,7 +94,7 @@ if (os.type() === 'Darwin') {
         // Wrap JSON data with Javascript multiline string variable named `csdJson`.
         let output = 'const csdJson = `\n    ' + jsonData + '\n    `'
 
-            // Update BabylonJs/project.ts csdJson variable.
+        // Update BabylonJs/project.ts csdJson variable.
         let data = fs.readFileSync(babylonJsDir + '/project.ts', 'ascii')
 
         data = data.replace(new RegExp('const csdJson = `[^`]*`', 'g'), output)
