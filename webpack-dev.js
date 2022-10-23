e74f81684a5be113621242c99ca739fc97665a42
Dr. Ewww <andy@docEdub.com>
2022-10-22 21:53:06 -0400

    Change webpack dev server port to 8081 to avoid recent Quest 2 issue
    See https://discord.com/channels/758943204715659264/1032680710315704351/1033002202941362236.

--- webpack-dev.js
+++ webpack-dev.js
@@ -89,7 +89,7 @@ module.exports = {
             '.github.com',
         ],
         contentBase: path.join(__dirname, 'app-dev'),
-        host: '0.0.0.0', port: 9000,
+        host: '0.0.0.0', port: 8081,
         inline: true,
         noInfo: false,
         mimeTypes: { typeMap: { 'text/javascript': [ 'js' ] }, force: true },
